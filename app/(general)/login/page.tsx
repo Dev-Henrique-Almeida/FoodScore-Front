7ee09276574abc6c9d7aa5a98e9c24f5fe44c55a
claudierio <claudierio@gmail.com>
2024-08-15 13:44:42 -0300

    feat: Adicionado conexão com a API na tela de Registro de usuario

--- app/(general)/login/page.tsx
+++ app/(general)/login/page.tsx
@@ -1,38 +1,42 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import axios from "axios";
 import styles from "./login.module.scss";
+import Link from "next/link";
 
 export default function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-  
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      try {
-        const response = await axios.post('http://localhost:3000/users/login', {
-          email,
-          password
-        });
-        console.log(response.data);
-        // Handle successful login
-      } catch (err) {
-        setError('Login falhou. Por favor, tente novamente.');
-      }
-    };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3000/users/login", {
+        email,
+        password,
+      });
+      console.log(response.data);
+      // Handle successful login
+    } catch (err) {
+      setError("Login falhou. Por favor, tente novamente.");
+    }
+  };
   return (
     <div className={styles.topLevel}>
       <div className={styles.container}>
         <h1 className={styles.title}>Login</h1>
         <form className={styles.form} onSubmit={handleSubmit}>
           <div className={styles.field}>
-            <label htmlFor="email" className={styles.label}>Email:</label>
+            <label htmlFor="email" className={styles.label}>
+              Email:
+            </label>
             <input
               type="email"
               id="email"
               name="email"
+              placeholder="email"
               required
               className={styles.input}
               value={email}
@@ -40,19 +44,34 @@ export default function Login() {
             />
           </div>
           <div className={styles.field}>
-            <label htmlFor="password" className={styles.label}>Password:</label>
+            <label htmlFor="Senha" className={styles.label}>
+              Password:
+            </label>
             <input
               type="password"
               id="password"
               name="password"
+              placeholder="Senha"
               required
               className={styles.input}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <button type="submit" className={styles.buttonSubmmit}>Login</button>
+          <button type="submit" className={styles.buttonSubmmit}>
+            Login
+          </button>
           {error && <p className={styles.error}>{error}</p>}
+
+          <div className={styles.inputGroup}>
+            <div className={styles.lostPass}>
+              <span>Esqueci minha senha</span>
+            </div>
+
+            <div className={styles.register}>
+              <Link href="/register" className={styles.regiterLink}>Registre-se</Link>
+            </div>
+          </div>
         </form>
       </div>
     </div>
--- app/(general)/register/page.tsx
+++ app/(general)/register/page.tsx
@@ -1,27 +1,102 @@
-import React from 'react'
-import styles from '../login/login.module.scss'
+"use client";
 
-export default function register(){
+import React, { useState } from "react";
+import axios from "axios";
+import styles from "../login/login.module.scss";
+import Link from "next/link";
+
+export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:3000/users", {
+        name,
+        email,
+        password,
+      });
+
+      setSuccess("Registro realizado com sucesso!");
+      if (response.status === 201) {
+        console.log(response.data);
+        const { name, token } = response.data;
+      }
+    } catch (err) {
+      setError("O registro falhou. Por favor, tente novamente.");
+      setSuccess("");
+    }
+  };
+
   return (
     <div className={styles.topLevel}>
       <div className={styles.container}>
         <h1 className={styles.title}>Registro</h1>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <div className={styles.field}>
-            <label htmlFor="name" className={styles.label}>Name:</label>
-            <input type="text" id="name" name="name" required className={styles.input} />
+            <label htmlFor="name" className={styles.label}>
+              Nome:
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Nome"
+              required
+              className={styles.input}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div className={styles.field}>
-            <label htmlFor="email" className={styles.label}>Email:</label>
-            <input type="email" id="email" name="email" required className={styles.input} />
+            <label htmlFor="email" className={styles.label}>
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              required
+              className={styles.input}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className={styles.field}>
-            <label htmlFor="password" className={styles.label}>Password:</label>
-            <input type="password" id="password" name="password" required className={styles.input} />
+            <label htmlFor="password" className={styles.label}>
+              Senha:
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Senha"
+              required
+              className={styles.input}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
-          <button type="submit" className={styles.buttonSubmmit}>Login</button>
+          <button type="submit" className={styles.buttonSubmmit}>
+            Registrar
+          </button>
+
+          {error && <p className={styles.error}>{error}</p>}
+          {success && <p className={styles.success}>{success}</p>}
+
+          <div className={styles.register}>
+            <span>
+              Já tem uma conta? <Link href="/login">Faça login</Link>
+            </span>
+          </div>
         </form>
       </div>
     </div>
-  )
+  );
 }
