24bf0b95ba22a4de0bb21b9636d03ff7c245f415
claudierio <claudierio@gmail.com>
2024-08-15 15:05:38 -0300

    style: Estilização do footer inicial

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,29 +1,60 @@
 import React from "react";
 import styles from "./footer.module.scss";
+import Image from "next/image";
+import Logo from "@/public/icons/logo.png";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.content}>
         <div className={styles.leftSection}>
-          <img src="/logo.png" alt="restaurant logo" className={styles.logo} />
+          <Image src={Logo} alt="logoFooter" className={styles.logo} />
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore. 
+            eiusmod tempor incididunt ut labore et dolore.
             <a href="#"> Learn more</a>
           </p>
         </div>
         <div className={styles.rightSection}>
           <h4>FOLLOW US</h4>
           <div className={styles.socialIcons}>
-            <a href="#"><img src="/facebook-icon.png" alt="Facebook" /></a>
-            <a href="#"><img src="/instagram-icon.png" alt="Instagram" /></a>
-            <a href="#"><img src="/twitter-icon.png" alt="Twitter" /></a>
+            <a href="#">
+              <FacebookIcon
+                sx={{
+                  width: "24px",
+                  height: "24px",
+                  color: "#000000",
+                }}
+              />
+            </a>
+            <a href="#">
+              <InstagramIcon
+                sx={{
+                  width: "24px",
+                  height: "24px",
+                  color: "#000000",
+                }}
+              />
+            </a>
+            <a href="#">
+              <TwitterIcon
+                sx={{
+                  width: "24px",
+                  height: "24px",
+                  color: "#000000",
+                }}
+              />
+            </a>
           </div>
         </div>
       </div>
       <div className={styles.bottomBar}>
-        <p>© 2024 Reviews. All Right Reserved. Designed by <strong>Tiago</strong></p>
+        <p>
+          © 2024 Reviews. All Right Reserved. Designed by <strong>Tiago</strong>
+        </p>
         <nav className={styles.navFooter}>
           <a href="#">Terms of Service</a>
           <a href="#">Privacy Policy</a>
--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -4,14 +4,13 @@ import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/public/icons/logo.png";
 import HomeIcon from "@mui/icons-material/Home";
-import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 
 export default function Navbar() {
     return(
         <div className={styles.container}>
         <div className={styles.toplevel}>
           <Link href="/">
-            <Image src={Logo} alt="navlogo teste" className={styles.logoImg} />
+            <Image src={Logo} alt="navLogo" className={styles.logoImg} />
           </Link>
           <nav className={styles.navbar}>
             <div className={styles.navleft}>
