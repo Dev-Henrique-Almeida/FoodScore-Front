d30654920e39ee0114d160faa78068ccda7e842a
Henrique Almeida <henriquesilvabcc@gmail.com>
2024-08-20 14:31:34 -0300

    feat: checando se a lista de restaurantes é nula ou nao.

--- app/shared/components/restaurant/index.tsx
+++ app/shared/components/restaurant/index.tsx
@@ -8,16 +8,20 @@ interface RestaurantsProps {
 const Restaurants: React.FC<RestaurantsProps> = ({ restaurants }) => {
   return (
     <div>
-      {restaurants.map((restaurant) => (
-        <div key={restaurant.id}>
-          <h2>{restaurant.name}</h2>
-          <p>{restaurant.address}</p>
-          <p>{restaurant.phone}</p>
-          {restaurant.image && (
-            <img src={restaurant.image} alt={restaurant.name} />
-          )}
-        </div>
-      ))}
+      {restaurants.length > 0 ? (
+        restaurants.map((restaurant) => (
+          <div key={restaurant.id}>
+            <h2>{restaurant.name}</h2>
+            <p>{restaurant.address}</p>
+            <p>{restaurant.phone}</p>
+            {restaurant.image && (
+              <img src={restaurant.image} alt={restaurant.name} />
+            )}
+          </div>
+        ))
+      ) : (
+        <p>Nenhum restaurante cadastrado.</p>
+      )}
     </div>
   );
 };
