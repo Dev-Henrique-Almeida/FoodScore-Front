ece59801c239e495b626d2ff56d77c5635f0183a
claudierio <claudierio@gmail.com>
2024-08-21 18:18:40 -0300

    feat: implementação inicial do carrosel nos card da home

--- app/shared/components/homepage/restaurant/RestaurantList.tsx
+++ app/shared/components/homepage/restaurant/RestaurantList.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import { IRestaurantData } from "@/app/shared/@types";
 import styles from "./restaurantList.module.scss";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 interface RestaurantListProps {
   restaurants: IRestaurantData[];
@@ -28,38 +30,83 @@ const RestaurantList: React.FC<RestaurantListProps> = ({ restaurants }) => {
       <h2 className={styles.title}>
         Relacionado aos restaurantes vistos por você
       </h2>
-      <div className={styles.restaurantGrid}>
+      <Carousel
+        showThumbs={false}
+        showStatus={false}
+        infiniteLoop
+        useKeyboardArrows
+        centerMode
+        centerSlidePercentage={30} 
+        swipeable
+        emulateTouch
+        dynamicHeight={false}
+        selectedItem={0}
+        interval={3000}
+        transitionTime={500}
+        autoPlay={false}
+        stopOnHover
+        renderArrowNext={(onClickHandler, hasNext, label) =>
+          hasNext && (
+            <div
+              className={styles.arrowNext}
+              onClick={onClickHandler}
+              title={label}
+            >
+              ▶
+            </div>
+          )
+        }
+        renderArrowPrev={(onClickHandler, hasPrev, label) =>
+          hasPrev && (
+            <div
+              className={styles.arrowPrev}
+              onClick={onClickHandler}
+              title={label}
+            >
+              ◁
+            </div>
+          )
+        }
+      >
         {restaurants.map((restaurant) => (
           <div
             key={restaurant.id}
-            className={styles.restaurantCard}
-            onClick={(event) => handleCardClick(event, restaurant.id)}
+            className={styles.restaurantCardWrapper}
           >
-            <img
-              src={restaurant.image || "/restaurant_default.jpg"}
-              alt={restaurant.name}
-              title={
-                restaurant.image
-                  ? ""
-                  : "Imagem padrão: A loja não inseriu uma foto própria."
-              }
-            />
-            <div className={styles.restaurantInfo}>
-              <h3>{restaurant.name}</h3>
-              <p>{restaurant.address}</p>
-              <p>{restaurant.phone}</p>
-              <div className={styles.restaurantRating}>
-                {Array(5)
-                  .fill("⭐")
-                  .map((star, index) => (
-                    <span key={index}>{star}</span>
-                  ))}
-                <span className={styles.reviewCount}>100 avaliações</span>
+            <div
+              className={styles.restaurantCard}
+              onClick={(event) => handleCardClick(event, restaurant.id)}
+            >
+              <img
+                src={restaurant.image || "/restaurant_default.jpg"}
+                alt={restaurant.name}
+                title={
+                  restaurant.image
+                    ? ""
+                    : "Imagem padrão: A loja não inseriu uma foto própria."
+                }
+              />
+              <div className={styles.restaurantInfo}>
+                <h3>{restaurant.name}</h3>
+                <p>{restaurant.address}</p>
+                <p>{restaurant.phone}</p>
+                <div className={styles.restaurantRating}>
+                  {Array(5)
+                    .fill("⭐")
+                    .map((star, index) => (
+                      <span key={index}>{star}</span>
+                    ))}
+                  <span className={styles.reviewCount}>100 avaliações</span>
+
+                </div>
               </div>
+            </div>
+            <div>
+              <p>teste</p>
             </div>
           </div>
         ))}
-      </div>
+      </Carousel>
     </div>
   );
 };
