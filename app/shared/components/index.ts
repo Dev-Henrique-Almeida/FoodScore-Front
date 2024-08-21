44f772fec3706f9047adb0d1db21d618c537183c
Henrique Almeida <henriquesilvabcc@gmail.com>
2024-08-21 10:07:36 -0300

    fix: correção da funcao de listar restaurantes.

--- app/shared/components/index.ts
+++ app/shared/components/index.ts
@@ -3,3 +3,5 @@ export * from "./navbar/index";
 export * from "./search";
 export * from "./inputs/customInput";
 export * from "./inputs/customSelect";
+export * from "./restaurant/restaurantContainer";
+export * from "./restaurant/index";
--- app/shared/service/api/RestaurantApi/index.ts
+++ app/shared/service/api/RestaurantApi/index.ts
@@ -1,10 +1,17 @@
 import { api, configHeaders } from "../api";
 
+import { AxiosError } from "axios";
+
 export const ListRestaurants = async () => {
   try {
     const response = await api.get(`/restaurants`, configHeaders);
     return response.data;
   } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response && error.response.status === 404) {
+        return [];
+      }
+    }
     throw error;
   }
 };
