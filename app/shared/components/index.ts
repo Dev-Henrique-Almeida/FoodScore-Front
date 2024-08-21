de52ab19c2e11c30e2b2cde539ab936bcdc9bfb2
Henrique Almeida <henriquesilvabcc@gmail.com>
2024-08-21 08:47:03 -0300

    refactor: refatorado os inputs.

--- app/shared/components/index.ts
+++ app/shared/components/index.ts
@@ -1,6 +1,5 @@
 export * from "./footer/index";
 export * from "./navbar/index";
 export * from "./search";
-export * from "./inputs/inputField";
 export * from "./inputs/customInput";
 export * from "./inputs/customSelect";
